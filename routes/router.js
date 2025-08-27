1d7adfd40337d770ad85e7c5d0f502ad5f5ba835
matiasStancato <stancato.m@gmail.com>
2025-08-27 20:39:32 +0200

    MODhome

--- routes/router.js
+++ routes/router.js
@@ -14,29 +14,33 @@ router.get('/contacto',(req,res) =>{
 router.get('/calendario',(req,res)=>{
     res.render("home/calendario")
 });
-
+//imagenes
 router.get('/imagenes',(req,res)=>{
     res.render("home/imagenes")
 });
-
+//instituciones
 router.get('/instituciones',(req,res)=>{
     res.render("home/instituciones")
 });
-
+//noticias
 router.get('/noticias',(req,res)=>{
     res.render("home/noticias")
 });
-
+//records
 router.get('/records',(req,res)=>{
     res.render("home/records")
 });
-
+//reglamentos
 router.get('/reglamento',(req,res )=>{
     res.render("home/reglamento")
 });
-
+//torneos
 router.get('/torneos',(req,res)=>{
     res.render("home/torneos")
+});
+//legales
+router.get('/legales',(req,res)=>{
+    res.render("home/legales")
 });
 
 export default router;
